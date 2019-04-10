9f47c565bd4ce33b17255b9a7aa8736281c05a1a
Bart Wood <bartwood@gmail.com>
2019-04-09 19:10:49 -0600

    stuff

--- src/api/App.js
+++ src/api/App.js
@@ -30,7 +30,7 @@ class AppBuilder {
 
 new AppBuilder(app);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
